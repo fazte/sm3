6fcebbcbfc3e8e2e34a979c405aed9b494b52d96
fazte <gafarchuk@icloud.com>
2023-09-08 12:06:40 +0300

    asd

---
+++ assets/js/js.js
@@ -0,0 +1,14 @@
+let cost = prompt('Стоимость товара');
+let hav = prompt('Сколько денег у вас');
+if((cost-hav) == 0){
+	alert ('Покупка совершена');
+}else{
+	if((cost-hav) > 0){
+		alert ('Для покупки не хватает ' + ' ' + (cost-hav) +' р.');
+	}else{
+			if((cost-hav) < 0){
+			alert('Покупка совершена. Сдача ' + ' ' + (hav-cost) +' р.');
+		}
+	
+	}
+}
